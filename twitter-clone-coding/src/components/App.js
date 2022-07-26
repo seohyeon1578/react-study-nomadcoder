2e3a7e044e40c45d1fe00e6dec168032a09d7734
seohyeon1578 <seohyeon1578@naver.com>
2022-07-26 11:52:14 +0900

    .

--- twitter-clone-coding/src/components/App.js
+++ twitter-clone-coding/src/components/App.js
@@ -18,7 +18,6 @@ function App() {
   return (
     <>
       {init ? <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj}/> : "Initializing...."}
-      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
   )
 }
--- twitter-clone-coding/src/routes/Home.js
+++ twitter-clone-coding/src/routes/Home.js
@@ -12,6 +12,7 @@ import Nweet from "../components/Nweet";
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState(); 
 
   useEffect(() => {
     const q = query(
@@ -45,11 +46,26 @@ const Home = ({ userObj }) => {
     setNweet(value);
   }
 
+  const onFileChange = e => {
+    const {target:{files}} = e;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: {result}
+      } = finishedEvent;
+      setAttachment()
+    }
+    reader.readAsDataURL(theFile);
+  }
+
   return(
     <div>
       <form onSubmit={onSubmit}>
         <input type="text" value={nweet} onChange={onChange} placeholder="What's on your mind?" maxLength={120}/>
+        <input type="file" accept="image/*" onChange={onFileChange}/>
         <input type="submit" value="Nweet"/>
+        {attachment && <img src={attachment} width="50px" height="50px"/>}
       </form>
       <div>
         {nweets.map((nweet) => (
